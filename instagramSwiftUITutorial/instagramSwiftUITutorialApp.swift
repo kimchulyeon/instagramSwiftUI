026203614f631072624e432b5c3c019a761f0760
chulyeon <guinness987@gmail.com>
2023-01-04 15:29:37 +0900

    Initial Commit

---
+++ instagramSwiftUITutorial/instagramSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagramSwiftUITutorialApp.swift
+//  instagramSwiftUITutorial
+//
+//  Created by chulyeon kim on 2023/01/04.
+//
+
+import SwiftUI
+
+@main
+struct instagramSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
